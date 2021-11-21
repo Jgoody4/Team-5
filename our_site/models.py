7683f02e4725b1329bb3918bad9719ceb177f634
Joel Zapana <joel.zapana@sjsu.edu>
2021-11-20 20:11:39 -0800

    Created a class for Flashcards that has a term and defintion for each card.

--- our_site/models.py
+++ our_site/models.py
@@ -1,1 +1,9 @@
 from our_site import db
+
+class FlashCard(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	card_term = db.Column(db.String(64), index=True, unique=True)
+	card_def = db.Column(db.String(128), index=True, unique=True)
+
+	def __repr__(self):
+		return f'Term: {self.card_term}, Definition: {self.card_def}'

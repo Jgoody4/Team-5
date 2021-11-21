861ede4deacd5b3a8f55e99ed6c198d172ee560a
Joel Zapana <joel.zapana@sjsu.edu>
2021-11-20 21:00:47 -0800

    Created a new function for forms that works as a shuffle button.

--- our_site/forms.py
+++ our_site/forms.py
@@ -5,3 +5,6 @@ from wtforms.validators import DataRequired
 class TimeInserted(FlaskForm):
 	inserted_time = StringField('Time', validators=[DataRequired()])
 	submit = SubmitField('Submit')
+
+class Shuffling(FlaskForm):
+	shuffler = SubmitField('Shuffle')
